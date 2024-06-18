8745c90837c09a3394bf5f1e94c5a10ca5bc3bf4
threedots <threedotsellipsis@protonmail.com>
2024-06-18 18:56:44 +0500

    Initial commit with simple nvim config

---
+++ nvim/.config/nvim/lua/plugins/vscode.lua
@@ -0,0 +1,7 @@
+return {
+    'Mofiqul/vscode.nvim',
+
+    config = function()
+        vim.cmd.colorscheme("vscode")
+    end
+}
---
+++ nvim/.config/nvim/options.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = ' '
+vim.g.localmapleader = ' '
+vim.g.have_nerd_font = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", {})
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", {})
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", {})
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", {})
+
+vim.keymap.set("i", "kj", "<Esc>", {})
