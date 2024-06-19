0fde681c87b727b05df29342a75ddaa44a77eb86
threedots <threedotsellipsis@protonmail.com>
2024-06-19 11:30:14 +0500

    Add completions and formatters to nvim

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -19,9 +19,13 @@ return {
         "neovim/nvim-lspconfig",
 
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
             local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', "gd", vim.lsp.buf.definition, {})
