05aa044bb50bc8c8af9cf6ecb2f6eac5cbb495ea
threedots <threedotsellipsis@protonmail.com>
2024-06-21 21:13:28 +0500

    Add comment plugin to nvim

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -11,6 +11,8 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.clipboard = vim.opt.clipboard + "unnamedplus"
 
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", {})
