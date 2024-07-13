9e244ed2e236553dba5e1b31bad8b63ba54a5adb
ThreeDots <threedotsellipsis@protonmail.com>
2024-07-13 12:48:53 +0500

    Remove unused config files

--- awesome/.config/awesome/custom/keyboard-layout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ `ibus engine` == "xkb:us::eng" ]]; then
-    ibus engine "anthy"
-elif [[ `ibus engine` == "anthy" ]]; then
-    ibus engine "xkb:ru::rus"
-else
-    ibus engine "xkb:us::eng"
-fi
