6c1f08c00a64da0d6b24fb2738f99a46b782e104
nickpinecone <nick.pineconefir@gmail.com>
2024-11-27 19:40:41 +0500

    Nvim neotree open file with xdg

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -8,6 +8,23 @@ return {
     },
 
     config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                window = {
+                    mappings = {
+                        ["o"] = "system_open",
+                    },
+                },
+            },
+            commands = {
+                system_open = function(state)
+                    local node = state.tree:get_node()
+                    local path = node:get_id()
+                    vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+                end,
+            },
+        })
+
         vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal left<CR>", {})
     end,
 }
